19e8ea47c06933d2fd8c5507e71518ecc4f7ddd0
carotproject <carotproject@gmail.com>
2023-12-20 17:28:20 -0500

    new commit

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-git pull
-git add *.py
-git add predicted_dataset 
-git commit -m "new commit"
-git branch -M main
-git remote add origin https://github.com/dadashkarimi/FPL-2023-2024.git
-git push -u origin main
